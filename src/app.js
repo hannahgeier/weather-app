6781111a9b240bd570a8b2b6337710beffbfe99b
hannahgeier <113505171+hannahgeier@users.noreply.github.com>
2022-10-05 23:38:30 +0200

    added unit conversion

--- src/app.js
+++ src/app.js
@@ -15,8 +15,10 @@ function formatDate (timestamp) {
 
 function displayTemperature (response) {
 
+celsiusTemperature = response.data.main.temp;
+
 let temperatureElement = document.querySelector("#temperature");
-temperatureElement.innerHTML = Math.round(response.data.main.temp);
+temperatureElement.innerHTML = Math.round(celsiusTemperature);
 
 let cityElement = document.querySelector("#city");
 cityElement.innerHTML = response.data.name;
@@ -50,5 +52,35 @@ let cityInputElement = document.querySelector ("#city-input");
 search (cityInputElement.value);
 }
 
+function displayFahrenheitTemperature(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+
+    celsiusLink.classList.remove("active");
+    fahrenheitLink.classList.add("active");
+
+    let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+
+    celsiusLink.classList.add("active");
+    fahrenheitLink.classList.remove("active");
+    
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+search("Sydney");
